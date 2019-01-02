eede23e77ba42f6512534c5943e83f1cffed0d2e
Oleg <kelmyashkin@gmail.com>
2019-01-02 15:39:33 +0300

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <AvailPicker></AvailPicker>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import Vue from 'vue';
+  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import AvailPicker from '@/components/AvailPicker/AvailPicker.vue';
 
-export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-});
+  export default Vue.extend({
+    name: 'home',
+    components: {
+      HelloWorld,
+      AvailPicker,
+    },
+  });
 </script>
